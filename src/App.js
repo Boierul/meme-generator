e098dddde2c54467ca48f07531e733ca2b303435
Boierul <49337867+Boierul@users.noreply.github.com>
2022-08-29 21:27:36 +0200

    Added MDB Footer (added dependencies in package.json). Minor tweaks.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from "./Navbar/Navbar";
 import Meme from "./Meme/Meme";
+import Footer from "./Footer/Footer";
 
 function App() {
     return (
         <div>
             <Navbar/>
             <Meme/>
+            <Footer/>
         </div>
     );
 }
